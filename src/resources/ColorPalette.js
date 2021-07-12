e371da80dd71275efccfa7f0ababa852774ac28a
Stan <hughes.stan@gmail.com>
2021-07-12 18:06:03 -0400

    Created Color Palette

---
+++ src/resources/ColorPalette.js
@@ -0,0 +1,6 @@
+const darkText = '#101219'
+const lightText = '#373E54'
+const paleButton = '#E2C3B6'
+const paleButtonPressed = '#E7CEC3'
+
+export { darkText, lightText, paleButton, paleButtonPressed }
